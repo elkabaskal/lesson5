5f463e49c6c46bd85730c01f8e8778878b4bdfe7
elkabaskal <audiopt@list.ru>
2021-08-27 10:53:42 +0500

    Add js code for table
    Нумерация и буквенные индексы встали неровно

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+//const myDiv = document.createElement('div');
+
+
+
+
+//myDiv.appendChild(myTable);
+
+
+
+
+/* function createTable() {
+    const myDiv = document.createElement('div');
+    const p = document.createElement('p');
+    p.textContent = 'Шахматная доска';
+    myDiv.appendChild(p);
+    document.body.appendChild(myDiv);
+    for (let i = 0; i < 9; i++) {
+        const row = document.createElement('div');
+        myDiv.appendChild(row);
+        for (let i = 0; i < 9; i++) {
+            const col = document.createElement('div');
+            row.appendChild(col);
+            col.style.border = '1px solid #000';
+        }
+    }
+}
+createTable(); */
